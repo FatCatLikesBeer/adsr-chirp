60247183cf18f4755683f5dd6ae74aad2a5e3bb3
Billy Pamanian <itisbilly@gmail.com>
2024-12-27 01:26:35 -0800

    Instrument plays repeatedly, Supabase select command working well

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
 import InstrumentOne from './library/InstrumentOne.ts';
 import './App.css';
 
-function App() {
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
 
+function App() {
   function createAndPlay() {
     new InstrumentOne();
   }
+
+  useEffect(() => {
+    supabase.from("posts").select()
+      .then((response) => {
+        console.log(response);
+      });
+  }, []);
 
   return (
     <>
